b93912cdbe9a4eac27d1be077d5924b444a368ee
Enhex <enhex0@gmail.com>
2017-11-18 21:38:30 +0200

    refactor building

--- build.bat
+++ build.bat
@@ -3,5 +3,4 @@ mkdir deco-benchmark-build
 cd deco-benchmark-build
 
 conan install ../deco-benchmark
-conan install ../deco-benchmark -s build_type=Debug
 cmake ../deco-benchmark -G "Visual Studio 15 Win64"
